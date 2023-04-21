b5eeb49e86a039fd5cb9bdcb4582f607f915a253
rajababu027 <rajababu12271998@gmail.com>
2023-04-21 16:28:44 +0530

    lazy loading using GetX

--- lazy_loading_test/lib/lazy_loading.dart
+++ lazy_loading_test/lib/lazy_loading.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lazy_loading_test/lazy_loading_controller.dart';
@@ -22,39 +21,43 @@ class LazyLoading extends GetView<LazyLoadingController> {
   }
 
   Widget _buildList() {
-    return ListView.builder(
-      itemCount: controller.users.length +
-          1, // Add one more item for progress indicator
-      padding: EdgeInsets.symmetric(vertical: 8.0),
-      itemBuilder: (BuildContext context, int index) {
-        if (index == controller.users.length) {
-          return _buildProgressIndicator();
-        } else {
-          return new ListTile(
-            leading: CircleAvatar(
-              radius: 30.0,
-              backgroundImage: NetworkImage(
-                controller.users[index]['picture']['large'],
+    return Obx(() {
+      return ListView.builder(
+        itemCount: controller.users.length +
+            1, // Add one more item for progress indicator
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == controller.users.length) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: Get.width,
+                height: Get.height * .8,
+                alignment: Alignment.center,
+                child: Opacity(
+                  opacity: controller.isLoading.value ? 1.0 : 00,
+                  child: const CircularProgressIndicator(
+                    backgroundColor: Colors.red,
+                    strokeWidth: 8,
+                  ),
+                ),
               ),
-            ),
-            title: Text((controller.users[index]['name']['first'])),
-            subtitle: Text((controller.users[index]['email'])),
-          );
-        }
-      },
-      controller: controller.sc,
-    );
-  }
-
-  Widget _buildProgressIndicator() {
-    return new Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Center(
-        child: new Opacity(
-          opacity: controller.isLoading.value ? 1.0 : 00,
-          child: new CircularProgressIndicator(),
-        ),
-      ),
-    );
+            );
+          } else {
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 30.0,
+                backgroundImage: NetworkImage(
+                  controller.users[index]['picture']['large'],
+                ),
+              ),
+              title: Text((controller.users[index]['name']['first'])),
+              subtitle: Text((controller.users[index]['email'])),
+            );
+          }
+        },
+        controller: controller.sc,
+      );
+    });
   }
 }
